f3e6c833ae44974d16f4e4a7edac77408ca2e736
ShurinO <arestarx@ukr.net>
2018-06-25 13:53:08 +0300

    LabWork_2.14.3

--- src/com/brainacad/oop/testgenerics2/Main.java
+++ src/com/brainacad/oop/testgenerics2/Main.java
@@ -8,10 +8,13 @@ public class Main {
         System.out.println("Array values: " + Arrays.toString(integers));
         int number = 3;
         System.out.println("Number of elements that are greater than "+number+": "+MyTestMethod.calcNum(integers,number));
+        System.out.println("Sum of elements that are greater than "+number+": "+MyTestMethod.calcSum(integers,number));
 
         Double[] doubles = {1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9};
         System.out.println("Array values: " + Arrays.toString(doubles));
         double number2 = 3;
         System.out.println("Number of elements that are greater than "+number2+": "+MyTestMethod.calcNum(doubles,number2));
+        System.out.println("Sum of elements that are greater than "+number2+": "+MyTestMethod.calcSum(doubles,number2));
+
     }
 }
--- src/com/brainacad/oop/testgenerics2/MyTestMethod.java
+++ src/com/brainacad/oop/testgenerics2/MyTestMethod.java
@@ -1,13 +1,71 @@
 package com.brainacad.oop.testgenerics2;
 
 public class MyTestMethod {
-    public static <T extends Number> int calcNum(T[] array, T maxElem){
-        int res = 0;
+    public static <T extends Number> long calcNum(T[] array, T maxElem){
+        long res = 0;
         for (T t : array) {
             if (t.doubleValue()>maxElem.doubleValue()){
                 res++;
             }
         }
         return res;
+    }
+
+    public static <T extends Number> Number calcSum(T[] array, T maxValue){
+        //так как в классе Number нет возможности проводить математические операции и возвращать числа в требуемом формате,
+        // то просто проверяем к какому классу реально относится T и уже с ним работаем и его возвращаем
+        switch (maxValue.getClass().getSimpleName()){
+            case "Double":
+                Double res = (double) 0;
+                for (T t : array) {
+                    if (t.doubleValue()>maxValue.doubleValue()){
+                        res+=t.doubleValue();
+                    }
+                }
+                return res;
+            case "Integer":
+                Integer res2 = 0;
+                for (T t : array) {
+                    if (t.intValue()>maxValue.intValue()){
+                        res2+=t.intValue();
+                    }
+                }
+                return res2;
+            case "Short":
+                Short res3 = 0;
+                for (T t : array) {
+                    if (t.shortValue()>maxValue.shortValue()){
+
+                        res3 = (short)(t.shortValue()+res3);
+                    }
+                }
+                return res3;
+            case "Byte":
+                Byte res4 = 0;
+                for (T t : array) {
+                    if (t.byteValue()>maxValue.byteValue()){
+
+                        res4 = (byte)(t.byteValue()+res4);
+                    }
+                }
+                return res4;
+            case "Long":
+                Long res5 = 0L;
+                for (T t : array) {
+                    if (t.longValue()>maxValue.longValue()){
+                        res5+=t.longValue();
+                    }
+                }
+                return res5;
+            case "Float":
+                Float res6 = 0f;
+                for (T t : array) {
+                    if (t.floatValue()>maxValue.floatValue()){
+                        res6+=t.floatValue();
+                    }
+                }
+                return res6;
+        }
+        return (double) 0;
     }
 }
